a844de9f001796c30d8ec32832bbd1d187558757
Vaza <raisvaza@gmail.com>
2023-10-17 09:52:16 +0700

    fix: add ChartDashboard to water-level page

--- src/pages/water-level.js
+++ src/pages/water-level.js
@@ -1,12 +1,40 @@
+import ChartDashboard from "@/components/ChartDashboard"
 import LineChart from "@/components/LineChart"
+import Map from "@/components/Map"
 import Sidebar from "@/components/Sidebar"
-import { Flex } from "@chakra-ui/react"
+// import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { Flex, Select } from "@chakra-ui/react"
 
 export default function WaterLevel(){
     return (
-        <Flex>
+        <Flex flexDir="row"
+        >
             <Sidebar/>
-            <LineChart title={"Water Level"}/>
+            {/* <Flex flexDir="column">
+                <Select>
+                    <option>Pos Duga Air 1</option>
+                </Select>
+            </Flex> */}
+            <Flex
+                flexDir="column"
+                flexGrow={1}
+                >
+                    {/* <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[51.505, -0.09]}>
+                            <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    </MapContainer> */}
+                    {/* <Map/> */}
+                    <ChartDashboard/>
+            </Flex>
+            
+            
         </Flex>
         
     )
