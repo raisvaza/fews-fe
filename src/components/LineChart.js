002a7b34a90329d9b09242fedd6ec4f245c5cc97
Vaza <raisvaza@gmail.com>
2023-10-17 11:40:47 +0700

    fix: move data processing from LineChart component to water-level page

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,6 +1,6 @@
 import { Flex } from '@chakra-ui/react';
 
-import { faker } from '@faker-js/faker';
+
 
 import {
     Chart as ChartJS,
@@ -16,6 +16,7 @@ import {
 import { Line } from 'react-chartjs-2';
 
 import Sidebar from './Sidebar';
+import { useState } from 'react';
 
 ChartJS.register(
     CategoryScale,
@@ -27,77 +28,15 @@ ChartJS.register(
     Legend
 );
 
-// Define the initial date and an array to store the generated dates
-const startDate = new Date(); // Use the current date and time as the starting point
-const dates = [];
+export default function LineChart({flexGrow, data, options}) {
 
-// Generate 7 dates with a 15-minute difference
-for (let i = 0; i < 7; i++) {
-  // Create a new date by adding 15 minutes (in milliseconds) to the previous date
-  const newDate = new Date(startDate.getTime() + i * 15 * 60 * 1000);
-  const year = newDate.getFullYear();
-  const month = String(newDate.getMonth() + 1).padStart(2, '0'); // Month is zero-based
-  const day = String(newDate.getDate()).padStart(2, '0');
-  const hour = String(newDate.getHours()).padStart(2, '0');
-  const minute = String(newDate.getMinutes()).padStart(2, '0');
+  // const [data, setData] = useState(data);
 
-  const formattedDate = `${year}/${month}/${day} ${hour}:${minute}`;
-  dates.push(formattedDate);
-}
-
-// Print the generated dates
-dates.forEach((date, index) => {
-  console.log(`Date ${index + 1}: ${date}`);
-});
-
-
-
-const data = {
-  labels: dates,
-  datasets: [
-    {
-      label: 'Sungai A',
-      data: dates.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-      borderColor: 'rgb(255, 99, 132)',
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-    // {
-    //   label: 'Dataset 2',
-    //   data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-    //   borderColor: 'rgb(53, 162, 235)',
-    //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    // },
-  ],
-};
-
-export default function LineChart({title, flexGrow}) {
-    const options = {
-        responsive: true,
-        layout: {
-            padding: {
-                left: 100,
-                right: 100,
-                top: 20,
-                bottom: 20
-            },
-            
-        },
-        scales: {
-            y: {
-                suggestedMin: 0,
-                suggestedMax: 1000
-            }
-        },
-        plugins: {
-            legend: {
-            position: 'top',
-            },
-            title: {
-            display: true,
-            text: title,
-            },
-        },
-    };
+  // const filteredData = {
+  //   labels: data.labels,
+  //   datasetes: data.datasets.filter((item) => item.label == activeFilterOption)
+  // }
+    
     return (
         <Flex
             flexDir="column"
@@ -109,7 +48,7 @@ export default function LineChart({title, flexGrow}) {
             width="100vh">
                 
             </Flex> */}
-            <Line options={options} data={data} />;
+            <Line options={options} data={data} />
             
         </Flex>
     )
--- src/pages/water-level.js
+++ src/pages/water-level.js
@@ -1,13 +1,90 @@
 import ChartDashboard from "@/components/ChartDashboard"
 import LineChart from "@/components/LineChart"
-import Map from "@/components/Map"
+// import Map from "@/components/Map"
 import Sidebar from "@/components/Sidebar"
 // import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import { Flex, Select } from "@chakra-ui/react"
+import { faker } from '@faker-js/faker'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function WaterLevel(){
+    
+
+    // Define the initial date and an array to store the generated dates
+    const startDate = new Date(); // Use the current date and time as the starting point
+    const dates = [];
+
+    // Generate 7 dates with a 15-minute difference
+    for (let i = 0; i < 7; i++) {
+    // Create a new date by adding 15 minutes (in milliseconds) to the previous date
+    const newDate = new Date(startDate.getTime() + i * 15 * 60 * 1000);
+    const year = newDate.getFullYear();
+    const month = String(newDate.getMonth() + 1).padStart(2, '0'); // Month is zero-based
+    const day = String(newDate.getDate()).padStart(2, '0');
+    const hour = String(newDate.getHours()).padStart(2, '0');
+    const minute = String(newDate.getMinutes()).padStart(2, '0');
+
+    const formattedDate = `${year}/${month}/${day} ${hour}:${minute}`;
+    dates.push(formattedDate);
+    }
+
+    const title = "Water Level"
+
+    const waterLevelData = [
+        {
+            label: 'Pos Duga Air A',
+            data: dates.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+            borderColor: 'rgb(255, 99, 132)',
+            // backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          },
+          {
+            label: 'Pos Duga Air B',
+            data: dates.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+          },
+    ]
+
+    const filterOptions = waterLevelData.map((item) => item.label)
+
+    const data = {
+        labels: dates,
+        datasets: waterLevelData,
+    };
+    
+    const options = {
+        responsive: true,
+        layout: {
+            padding: {
+                left: 100,
+                right: 100,
+                top: 20,
+                bottom: 20
+            },
+            
+        },
+        scales: {
+            y: {
+                suggestedMin: 0,
+                suggestedMax: 1000
+            }
+        },
+        plugins: {
+            legend: {
+            position: 'top',
+            },
+            title: {
+            display: true,
+            text: title,
+            },
+        },
+    };
+
+
     return (
-        <Flex flexDir="row"
+        <Flex flexDir="row" className={`${inter.className}`}
         >
             <Sidebar/>
             {/* <Flex flexDir="column">
@@ -31,7 +108,8 @@ export default function WaterLevel(){
                         </Marker>
                     </MapContainer> */}
                     {/* <Map/> */}
-                    <ChartDashboard/>
+                    
+                    <ChartDashboard title={title} data={data} options={options} filterOptions={filterOptions}/>
             </Flex>
             
             
