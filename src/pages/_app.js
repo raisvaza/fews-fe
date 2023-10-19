d52ee64075d501f4736240141f5782428d2b4741
Vaza <raisvaza@gmail.com>
2023-10-19 16:07:32 +0700

    feat: consume Pos API in App

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,26 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import axios from 'axios'
+import { useState, useEffect } from 'react';
 
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
+  const [pos, setPos] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/get-pos/?format=json') 
+      .then((response) => setPos(response.data))
+      .catch((error) => console.error(error));
+    
+  },[])
+
+  // useEffect(() => {
+  //   console.log("IN APP:", pos)
+  // }, pos)
+
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <Component {...pageProps} posData={pos} />
     </ChakraProvider>
       
   )
